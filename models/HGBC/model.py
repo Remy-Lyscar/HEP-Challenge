ca0c5533ca9cce63493415e81983145eb0d4d0da
Remy-Lyscar <remy.lyscar@orange.fr>
2024-03-25 10:39:04 +0100

    minor to save_plots in atlas1 during training with codabench

--- models/HGBC/model.py
+++ models/HGBC/model.py
@@ -112,7 +112,7 @@ class Model():
         self.mu_hat_calc()
         self._validate()
         self._compute_validation_result()
-        # self._theta_plot()
+        self._theta_plot()
         self._save_model()
 
     def predict(self, test_set):
